86ba2fb38afd56c8546bd892d7825963994bd571
JanseR33 <janserratlabrat@gmail.com>
2025-10-08 11:07:18 +0200

    Update Program.cs

--- ComposicioCondicional/Lliurament_1_4/Ex08/Program.cs
+++ ComposicioCondicional/Lliurament_1_4/Ex08/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola soy Triline"); 
+            Console.WriteLine("among us"); 
         }
         }
     }

fb48700389efa11a48a824a22bb82669d4c37106
JanseR33 <janserratlabrat@gmail.com>
2025-10-09 09:18:22 +0200

    Move Ex01 and add Ex02 to BuclesWhile project
    Renamed Ex01 files and solution from ComposicioCondicional to ComposicioIterativa_BuclesWhile. Added new Ex02 project and implementation to the BuclesWhile solution.

---
+++ ComposicioIterativa_BuclesWhile/Lliurament_1_5/Ex02/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string NOM_FITXER = "../Fitxers/Numeros.txt";
+
+            int numElementsEntrats = 0;
+            int elementActual = 0;
+            StreamReader fitxerNumeros;
+
+            fitxerNumeros = new StreamReader(NOM_FITXER);
+
+            Console.WriteLine("Introdueix els elements fins acabar introduint un zero:");
+            elementActual = int.Parse(Console.ReadLine());
+            numElementsEntrats++;
+
+            while (elementActual != 0)
+            {
+                elementActual = int.Parse(Console.ReadLine());
+                numElementsEntrats++;
+            }
+
+            Console.WriteLine($"El total de números entrats ha estat de {numElementsEntrats}");
+        }
+    }
+}

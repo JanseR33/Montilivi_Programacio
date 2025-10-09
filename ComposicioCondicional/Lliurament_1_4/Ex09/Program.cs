e1bc03188e5915b70a71f185dd1dbd4e29ef6b57
JanseR33 <janserratlabrat@gmail.com>
2025-10-09 23:48:19 +0200

    Add Ex09 project for qualitative grade calculation
    Introduces the Ex09 project to the solution, including its .csproj and Program.cs files. The program calculates a qualitative grade based on exam and practical scores, applying specific thresholds and rules for final assessment.

---
+++ ComposicioCondicional/Lliurament_1_4/Ex09/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Ex09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introdueix la nota de les pràctiques");
+            double notaPractiques = double.Parse(Console.ReadLine());
+            Console.WriteLine("Introdueix la nota de l'examen");
+            double notaExamen = double.Parse(Console.ReadLine());
+
+            string qualitat = NotaQualitativa(notaExamen, notaPractiques);
+            Console.WriteLine($"La nota qualitativa és: {qualitat}");
+        }
+
+        public static string NotaQualitativa(double notaExamen, double notaPractiques)
+        {
+            const double PERCENTATGE_EXAMEN = 0.8;
+            const double PERCENTATGE_PRACTIQUES = 0.2;
+
+            if (notaExamen < 3 || notaPractiques < 3)
+            {
+                return "Suspens";
+            }
+
+            double notaFinalAssignatura =
+                (notaExamen * PERCENTATGE_EXAMEN) + (notaPractiques * PERCENTATGE_PRACTIQUES);
+
+            const double EPS = 1e-9;
+            if (Math.Abs(notaFinalAssignatura - 10.0) < EPS)
+                return "Matrícula d’honor";
+
+            if (notaFinalAssignatura < 5)
+                return "Suspens";
+            if (notaFinalAssignatura < 7)
+                return "Aprovat";
+            if (notaFinalAssignatura < 9)
+                return "Notable";
+
+            return "Excel·lent";
+        }
+    }
+}

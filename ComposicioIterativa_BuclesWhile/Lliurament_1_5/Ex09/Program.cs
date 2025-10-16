cd7ea6547a5e72370daf6a6be4757c1a1eb5e97a
JanseR33 <janserratlabrat@gmail.com>
2025-10-16 11:24:51 +0200

    Add Ex09 and Ex10 projects to solution
    Created Ex09 and Ex10 projects with initial Program.cs files. Ex09 counts the digits in a positive integer, and Ex10 simulates rolling a six-sided die until the number of 1s and 6s rolled are equal. Updated the solution file to include both new projects.

---
+++ ComposicioIterativa_BuclesWhile/Lliurament_1_5/Ex09/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex09
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Programa que al entrar un número enter positiu et diu quantes xifres té.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entra un nombre enter i et diré quantes xifres té");
+
+            int n = int.Parse(Console.ReadLine());
+
+            int numFinal = n;
+            int xifres = 0;
+
+            while (numFinal > 0)
+            {
+                xifres++;
+                numFinal /= 10;
+            }
+
+            Console.WriteLine($"Té {xifres} xifres.");
+        }
+    }
+}
---
+++ ComposicioIterativa_BuclesWhile/Lliurament_1_5/Ex10/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Ex10
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Fes un programa on llancem un dau de 6 cares (valors de forma aleatoria)
+        /// fins que el número de vegades que surti 1 i 6 sigui el mateix.
+        /// Conta quantes vegades hem llançat el dau i quantes vegades ha aparegut el valor 6. 
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            int quantitat1 = 0;
+            int quantitat6 = 0;
+            int quantitatD6Llançats = 0;
+            Console.WriteLine("Aquest programa llença un dau de 6 cares i diu quants cops ha caigut en 1 i quants en 6.");
+            int d6;
+
+            while (!((quantitat1 != quantitat6) && (quantitat1 != 0) && (quantitat6 != 0)))
+            {
+                quantitatD6Llançats++;
+                d6 = Random.Shared.Next(1, 7);
+                if (d6 == 1)
+                {
+                    quantitat1++;
+                }
+                else if (d6 == 6)
+                {
+                    quantitat6++;
+                }
+            }
+
+            Console.WriteLine($"La quantiatat de cops que s'ha llençat el D6 és de {quantitatD6Llançats}, mentre que la quantitat de 6 que han sortit ha estat de {quantitat6}");
+        }
+    }
+}

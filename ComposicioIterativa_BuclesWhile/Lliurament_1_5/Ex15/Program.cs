97e4cc6f0768f2a8733b9b5c91b693007cad46e6
JanseR33 <janserratlabrat@gmail.com>
2025-10-27 22:38:01 +0100

    Add Ex15 project for Girona match statistics
    Introduces the Ex15 project to the solution, including its .csproj and Program.cs. The program reads match data from a file and calculates Girona's wins, losses, draws, and total points for the season.

---
+++ ComposicioIterativa_BuclesWhile/Lliurament_1_5/Ex15/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Ex15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader partitsLliga = new StreamReader("Girona lliga23_24_v2.txt");
+
+            int golRival = 0, golGir = 0;
+            int puntuacioTotal = 0;
+            int partitsGuanyants = 0, partitsPerduts = 0, partitsEmpat = 0;
+            string linea;
+
+            linea = partitsLliga.ReadLine();
+            Console.WriteLine(linea);
+
+            if (linea != null)
+            {
+                golRival = Convert.ToInt32(linea);
+                linea = partitsLliga.ReadLine();
+            }
+
+            while (linea != null)
+            {
+                if (linea != null)
+                {
+                    golGir = Convert.ToInt32(linea);
+
+                    if (golGir > golRival)
+                    {
+                        partitsGuanyants++;
+                        puntuacioTotal += 3;
+                    }
+                    else if (golGir < golRival) partitsPerduts++;
+                    else
+                    {
+                        partitsEmpat++;
+                        puntuacioTotal += 1;
+                    }
+
+                    linea = partitsLliga.ReadLine();
+                }
+
+                if (linea != null)
+                {
+                    golRival = Convert.ToInt32(linea);
+                    linea = partitsLliga.ReadLine();
+                }
+            }
+
+            Console.WriteLine($"el total de partits que han guanyat el Girona es de: {partitsGuanyants}");
+            Console.WriteLine($"el total de partits que hna perdut el Girona es de: {partitsPerduts}");
+            Console.WriteLine($"el total de partits que han enpatat el Girona es de es de: {partitsEmpat}");
+            Console.WriteLine($"el total de punts fets del Girona es de {puntuacioTotal}");
+        }
+    }
+}

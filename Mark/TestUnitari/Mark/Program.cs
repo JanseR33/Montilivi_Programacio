9574501ee99e03d5ecd947a3beaf6ecd77035d95
JanseR33 <janserratlabrat@gmail.com>
2025-12-09 11:10:13 +0100

    Add Mark project with grade conversion logic
    Initial commit for the Mark console application. Includes a method to convert numeric grades to text descriptions and basic project setup files.

---
+++ Mark/TestUnitari/Mark/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Mark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ENTRA UNA NOTA: ");
+            int nota = Convert.ToInt32(Console.ReadLine());
+            string notaText = CalcularNotaText(nota);
+            Console.WriteLine(notaText);
+        }
+
+        /// <summary>
+        /// Transforma una nota numèrica en una nota en format text.
+        /// </summary>
+        /// <param name="nota">Nota numèrica entera.</param>
+        /// <returns>Nota en format text</returns>
+        /// <exception cref="Exception">La nota numèrica ha de ser un valor de l'interval [0,1]</exception>
+        public static string CalcularNotaText(int nota)
+        {
+            if (nota < 0 || nota > 10)
+                throw new Exception("Nota no vàlida");
+
+            string notaStr = "";
+            if (nota > 8) notaStr = "Excel·lent";
+            else if (nota > 6) notaStr = "Notable";
+            else if (nota > 4) notaStr = "Satisfactori";
+            else notaStr = "No assolit";
+
+            return notaStr;
+        }
+    }
+}
